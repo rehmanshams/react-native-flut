371474ddd1a733ef98249483d551bad8bcbdc4fd
rehmanshams <rehman.alphasquad@gmail.com>
2024-07-29 19:16:51 +0500

    added scanner

---
+++ Scan.tsx
@@ -0,0 +1,57 @@
+import React, {useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import {RNCamera} from 'react-native-camera';
+
+const App = () => {
+  const [data, setData] = useState('Scan Something');
+  return (
+    <QRCodeScanner
+      onRead={({data}) => setData(data)} // Assuming setData is the correct function to update state
+      reactivate={true}
+      reactivateTimeout={500}
+      showMarker={true} // or showMarker if you want it to be true
+      topContent={
+        <View>
+          <Text
+            style={{
+              color: 'black',
+              padding: 20,
+              fontSize: 20,
+              backgroundColor: 'grey',
+              margin: 10,
+            }}>
+            {data}
+          </Text>
+        </View>
+      }
+      bottomContent={
+        <View>
+          <Text>Qr Code Scanner</Text>
+        </View>
+      }
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
+});
+
+export default App;
